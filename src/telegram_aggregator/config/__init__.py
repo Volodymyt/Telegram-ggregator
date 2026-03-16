cbbe1c6ceef5b9e42deabc5131a1da6289e8ee99
Sevastianov Andrii <mrpkmail@gmail.com>
2026-03-16 19:58:57 +0000

    config: load filter groups from yaml

--- src/telegram_aggregator/config/__init__.py
+++ src/telegram_aggregator/config/__init__.py
@@ -7,11 +7,27 @@ from telegram_aggregator.config.app_config import (
     TelegramSessionSettings,
     load_app_config,
 )
+from telegram_aggregator.config.file_config import (
+    FileConfig,
+    FileConfigError,
+    FilterGroupConfig,
+    IncludeRuleConfig,
+    RepostConfig,
+    RuntimeConfig,
+    load_file_config,
+)
 
 __all__ = [
     "AppConfig",
     "AppConfigError",
     "ConfigPathError",
+    "FileConfig",
+    "FileConfigError",
+    "FilterGroupConfig",
+    "IncludeRuleConfig",
+    "RepostConfig",
+    "RuntimeConfig",
     "TelegramSessionSettings",
     "load_app_config",
+    "load_file_config",
 ]
